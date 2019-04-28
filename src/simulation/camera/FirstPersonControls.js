cf4523007b5a25fd9ec0bc6f123d4e859c8a49d0
Sergiu Bucur <sergiu.bucur@outlook.com>
2019-04-28 12:10:21 +0300

    keyboard rotation

--- src/simulation/camera/FirstPersonControls.js
+++ src/simulation/camera/FirstPersonControls.js
@@ -5,6 +5,10 @@ import Keybinds from "../input/Keybinds";
 export default class FirstPersonControls {
 	camera = null;
 	position = new THREE.Vector3(0, 5, 15);
+	forward = new THREE.Vector3(0, 0, -1);
+	right = new THREE.Vector3(1, 0, 0);
+	rotationX = 0;
+	rotationY = 0;
 	speed = 0.25;
 
 	constructor(camera, inputTracker) {
@@ -17,28 +21,31 @@ export default class FirstPersonControls {
 	}
 
 	update() {
+		this.updateRotation();
+
 		const velocity = this.getVelocity();
 
-		this.updatePosition(velocity);
+		const newPosition = this.position.clone().add(velocity);
+		this.updatePosition(newPosition);
 	}
 
 	getVelocity() {
 		const velocity = new THREE.Vector3();
 
 		if (this.inputTracker.keysPressed[Keybinds.MoveForward]) {
-			velocity.add(new THREE.Vector3(0, 0, -1));
+			velocity.add(this.forward);
 		}
 
 		if (this.inputTracker.keysPressed[Keybinds.MoveBackward]) {
-			velocity.add(new THREE.Vector3(0, 0, 1));
+			velocity.sub(this.forward);
 		}
 
 		if (this.inputTracker.keysPressed[Keybinds.StrafeLeft]) {
-			velocity.add(new THREE.Vector3(-1, 0, 0));
+			velocity.sub(this.right);
 		}
 
 		if (this.inputTracker.keysPressed[Keybinds.StrafeRight]) {
-			velocity.add(new THREE.Vector3(1, 0, 0));
+			velocity.add(this.right);
 		}
 
 		velocity.multiplyScalar(this.speed);
@@ -46,8 +53,37 @@ export default class FirstPersonControls {
 		return velocity;
 	}
 
-	updatePosition(velocity) {
-		const newPosition = this.position.clone().add(velocity);
+	updateRotation() {
+		if (this.inputTracker.keysPressed[Keybinds.Up]) {
+			this.rotationX -= 0.01;
+		}
+
+		if (this.inputTracker.keysPressed[Keybinds.Down]) {
+			this.rotationX += 0.01;
+		}
+
+		if (this.inputTracker.keysPressed[Keybinds.Left]) {
+			this.rotationY += 0.01;
+		}
+
+		if (this.inputTracker.keysPressed[Keybinds.Right]) {
+			this.rotationY -= 0.01;
+		}
+
+		const rotation = new THREE.Euler(this.rotationX, this.rotationY, 0, "XYZ");
+
+		this.forward.set(0, 0, -1);
+		this.forward.applyEuler(rotation);
+
+		rotation.set(0, this.rotationY, 0, "XYZ");
+
+		this.right.set(1, 0, 0);
+		this.right.applyEuler(rotation);
+
+		this.camera.lookAt(this.forward.clone().add(this.position));
+	}
+
+	updatePosition(newPosition) {
 		this.position.copy(newPosition);
 		this.camera.position.copy(this.position);
 	}
--- src/simulation/input/Keybinds.js
+++ src/simulation/input/Keybinds.js
@@ -2,5 +2,9 @@ export default {
 	MoveForward: 87,
 	MoveBackward: 83,
 	StrafeLeft: 65,
-	StrafeRight: 68
+	StrafeRight: 68,
+	Up: 38,
+	Down: 40,
+	Left: 37,
+	Right: 39
 };
