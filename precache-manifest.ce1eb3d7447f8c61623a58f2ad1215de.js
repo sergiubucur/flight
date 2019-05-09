dea26c37f9cc2e330e48fa327cff930c7bdf1a80
Sergiu Bucur <sergiu.bucur@outlook.com>
2019-05-09 21:33:57 +0300

    Updates

---
+++ precache-manifest.ce1eb3d7447f8c61623a58f2ad1215de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3324a4d80f283552756681e84fece090",
+    "url": "/flight/index.html"
+  },
+  {
+    "revision": "d1e0dee49c4de524e1f4",
+    "url": "/flight/static/css/main.fbfdb650.chunk.css"
+  },
+  {
+    "revision": "6dea8202709a08bbb5ef",
+    "url": "/flight/static/js/2.3732e362.chunk.js"
+  },
+  {
+    "revision": "d1e0dee49c4de524e1f4",
+    "url": "/flight/static/js/main.678770f4.chunk.js"
+  },
+  {
+    "revision": "4beefbc4bade265a0b81",
+    "url": "/flight/static/js/runtime~main.0139e3eb.js"
+  }
+]);
